4ece5020e78d0c26b3cdfcba83ee451ccd8242a3
LUJAIN ALKANHAL <lujain.i.k20@gmail.com>
2023-05-17 13:20:32 +0300

    Initial Commit

---
+++ DiolingoApp/DiolingoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiolingoAppApp.swift
+//  DiolingoApp
+//
+//  Created by لجين إبراهيم الكنهل on 27/10/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct DiolingoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
